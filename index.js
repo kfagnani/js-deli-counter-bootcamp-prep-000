b97a5e0b2252dac087804f4186b3ea9aa515a8ec
Kirstyn Fagnani <kirstyn.fagnani@gmail.com>
2019-07-11 19:24:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
-function takeANumber (people,name) {
+function takeANumber(line, name){
   line.push(name)
-  return 'Welcome, ${name}. You are number ${line.length} in line.'
+  return `Welcome, ${name}. You are number ${line.length} in line.`
 }
 
-function nowServing(line) {
+function nowServing(line){
   if(line.length === 0){
-    return 'There is nobody waiting to be served!'
-  }
-  else {
-    var upNext = line [0]
-    line.shift()
-    return "Currently serving ${upNext}."
-  }
-}
-
-function currentLine (line) {
-  var current = "The line is currently:"
-  if (line.length ===0) {
-    return "The line is currently empty"
+    return "There is nobody waiting to be served!"
   }
   else{
-    for(var i=0; i<line.length - 1; i ++){
-      current = current + '${i=1}. ${line[i]},'
-    }
-  return current + '${line.length}. ${line [ line.length-1]}0'
-    
+    var upNext = line[0]
+    line.shift()
+    return `Currently serving ${upNext}.`
   }
-  
+}
+
+function currentLine(line){
+  var current = "The line is currently: "
+  if (line.length === 0){
+    return "The line is currently empty."
+  }
+  else{
+    for(var i=0; i<line.length-1; i++){
+      current = current + `${i+1}. ${line[i]}, `
+    }
+    return current + `${line.length}. ${line[line.length -1]}`
+  }
 }
 
