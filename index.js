bceb59be0f1d544799096d1539d3e87a37c2ffb6
Kirstyn Fagnani <kirstyn.fagnani@gmail.com>
2019-07-11 19:18:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ function takeANumber (people,name) {
   return 'Welcome, ${name}. You are number ${line.length} in line.'
 }
 
+function nowServing(line) {
+  if(line.length === 0){
+    return 'There is nobody waiting to be served!'
+  }
+  
+}
