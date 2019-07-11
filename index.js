6327a46defa64ef1d8001822cea151be03078d5f
Kirstyn Fagnani <kirstyn.fagnani@gmail.com>
2019-07-11 20:31:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ var katzDeliLine = [];
 
 function takeANumber (line,person) {
   line.push(person)
-  return 'Welcome,' + person + '. You are number' + line.length =
+  return 'Welcome,' + person + '. You are number' + line.length + 'in line.'
 }
+
